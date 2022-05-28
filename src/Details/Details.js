a09d5369c8a2633bb73160d116edb336c604d9db
hemaith-uddin <hemaithisifat@gmail.com>
2022-05-28 19:56:16 +0600

    update quantity

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
+
+import { useParams } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../Firebase/fire.base.init';
 import { useForm } from "react-hook-form";
-import { useParams } from 'react-router-dom'; 
 import "./Details.css"
 
 const Details = ({ children }) => {
+    const [user] = useAuthState(auth);
     const { id } = useParams()
     const [details, setDetails] = useState({});
     useEffect(() => {
@@ -12,16 +16,33 @@ const Details = ({ children }) => {
             .then(res => res.json())
             .then(data => setDetails(data))
     }, [])
+
+
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data); 
-           const [count,setCount] = useState('1')
-          const handleQuantity =(e)=>{ 
-                setCount(+e.target.value) 
-                 
-          }
-            const orderPurchase =()=>{ 
-                window.confirm("are You sure want to purchase?")
-            }
+    const onSubmit = (data) => {
+        console.log(data);
+        fetch('http://localhost:5000/order', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert('Success:', data);
+            })
+    }
+
+
+    const [count, setCount] = useState('1')
+    const handleQuantity = (e) => {
+        setCount(+e.target.value)
+
+    }
+    const orderPurchase = () => {
+        window.confirm("are You sure want to purchase?")
+    }
     return (
 
         <div className='container'>
@@ -35,30 +56,36 @@ const Details = ({ children }) => {
 
                 </div>
                 <div className="col-lg-6 ">
-                   <div className="forms"> 
-                         <div className="quantity"> 
-                         <h1> Quantity</h1> 
-                         <input onChange={handleQuantity} placeholder='Enter Quantity' className='w-50 ' type="number" />  
-                          <p style={{color:"red"}}> {count<details.qunatity && count>=50?" Yes you can order Now  ":"you have to be  ordered at least 50"} </p>
-                          <p style={{color:"red"}}> {count>=details.qunatity ?"product are not available":""} </p>
-                         </div>
-                   <form onSubmit={handleSubmit(onSubmit)}>
-                 
-                   
-                 
-                        <input placeholder='Name' {...register("Name", { required: true, })} /> <br />
-                        <input placeholder='Phone' type= "number" {...register("Phone")} /> <br />
-                        <input placeholder='Email' type="email" {...register("Email")} /> <br />
-                        <input onClick={()=>{orderPurchase()} } type="submit" disabled={count<=details.qunatity && count>=50 ? false:true } />
-                    </form>
-                   
-                   </div>
+                    <div className="forms">
+                        <div className="quantity">
+                            <h1> Quantity</h1>
+                            <input onChange={handleQuantity} placeholder='Enter Quantity' className='w-50 ' type="number" />
+                            <p style={{ color: "red" }}> {count < details.qunatity && count >= 50 ? " Yes you can order Now  " : "you have to be  ordered at least 50"} </p>
+                            <p style={{ color: "red" }}> {count >= details.qunatity ? "product are not available" : ""} </p>
+                        </div>
+                        <form onSubmit={handleSubmit(onSubmit)}>
 
+
+
+                            <input placeholder='Name' {...register("Name")} /> <br />
+                            <input placeholder='Phone' type="number" {...register("Phone")} /> <br />
+                            <input value={user.email} placeholder='Email' type="email" {...register("Email")} /> <br />
+                            <input onClick={() => { orderPurchase() }} type="submit" disabled={count <= details.qunatity && count >= 50 ? false : true} />
+                        </form>
+
+                    </div>
                 </div>
-
             </div>
         </div>
     );
-};
+}
+
+
+
+
+
+
+
+
 
 export default Details;
