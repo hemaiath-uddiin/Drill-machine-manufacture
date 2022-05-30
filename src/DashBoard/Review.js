e0c69d1cb1e361fa721ae7eb43926c58164b8828
hemaith-uddin <hemaithisifat@gmail.com>
2022-05-30 12:01:17 +0600

    review style add

--- src/DashBoard/Review.js
+++ src/DashBoard/Review.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form"; 
+import "./SingleReview.css"
 const Review = () => {
     const { register, handleSubmit, formState: { errors }, } = useForm();
     const onSubmit = (data)=>{ 
-        fetch('https://floating-tor-10144.herokuapp.com/order', {
+        fetch('http://localhost:5000/review', {
             method: 'POST', // or 'PUT'
             headers: {
                 'Content-Type': 'application/json',
@@ -16,31 +17,26 @@ const Review = () => {
             })
         
         
-        console.log(data);
+        console.log(data); 
+        
     }
     return (
         <div className='container'>
             <h2> Add Your Review</h2>
             <div className="row">
-                <div className="col-lg-8">
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <input {...register("firstName", { required: true, maxLength: 20  })} />
-                        {errors.firstName?.type === 'maxLength' && "First name is required"}
+                <div className="col-lg-8 review">
+                    <form className='review_input' onSubmit={handleSubmit(onSubmit)}>
+                        <input {...register("firstName", { required: true, maxLength: 1000  })} /> <br />
+                        {errors.firstName?.type === 'maxLength' && " You can type 100 characters"}
 
-                        <input  type="number" placeholder='rating' {...register("rating", { required: true ,min: 1, max: 5 })} />
-                        {errors.rating?.type === "max" && <p> sorry </p> }
+                        <input  type="number" placeholder='rating' {...register("rating", { required: true ,min: 1, max: 5 })} /> <br />
+                        {errors.rating?.type === "max" && <p style={{color:"red",fontSize:"20px"}}> sorry You cannot given rating above 5 </p> }
                         {errors.rating?.type === "min" &&  alert("hi")}
 
-                        <input type="submit" />
+                        <input className='inputReview' type="submit" />
                     </form>
 
-                    {/* <form onSubmit={handleSubmit(onSubmit)}>
-                        <input {...register("Description", { required: true, maxLength: 20 })} />
-                       
-                        <input type="number" {...register("rating", { required:true, min: 5, max: 6 })} /> 
-                         
-                        <input type="submit" />
-                    </form> */}
+                  
 
                 </div>
 
--- src/Reviews/Reviews.js
+++ src/Reviews/Reviews.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import './Reviewss.css'
 const Reviews = () => {
        const [review,setReview] = useState([]) 
        useEffect(()=>{ 
@@ -14,9 +14,9 @@ const Reviews = () => {
                        { 
                          review.map((review,_id)=>{ 
                              return( 
-                                 <div key={_id} className="col-lg-4"> 
-                                      <h1> Rating {review.rating} </h1> 
-                                       <p> comments {review.description}  </p>
+                                 <div key={_id} className="col-lg-4 rating_wraper"> 
+                                      <h1 className='rantin_num'> <span className='ranting'>Rating</span> {review.rating} </h1> 
+                                       <p className='rating_des'> <span className='comments'>comments:</span> {review.firstName}  </p>
                                  </div>
                              )
                          })
