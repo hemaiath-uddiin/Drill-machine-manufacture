963852cab050b22a47c4a937bf0e0cb21b55f688
hemaith-uddin <hemaithisifat@gmail.com>
2022-05-30 14:12:32 +0600

    add

--- src/DashBoard/Review.js
+++ src/DashBoard/Review.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useForm } from "react-hook-form"; 
-import "./SingleReview.css"
+import "./SingleReview.css" 
+
 const Review = () => {
-    const { register, handleSubmit, formState: { errors }, } = useForm();
+    const { register, handleSubmit,  formState: { errors }, } = useForm();
     const onSubmit = (data)=>{ 
         fetch('http://localhost:5000/review', {
             method: 'POST', // or 'PUT'
@@ -16,16 +17,18 @@ const Review = () => {
                 console.log( data);
             })
         
-        
+           
         console.log(data); 
         
-    }
+    } 
+
+    
     return (
         <div className='container'>
             <h2> Add Your Review</h2>
             <div className="row">
                 <div className="col-lg-8 review">
-                    <form className='review_input' onSubmit={handleSubmit(onSubmit)}>
+                    <form className='review_input' onSubmit={handleSubmit(onSubmit  )}>
                         <input {...register("firstName", { required: true, maxLength: 1000  })} /> <br />
                         {errors.firstName?.type === 'maxLength' && " You can type 100 characters"}
 
@@ -33,7 +36,7 @@ const Review = () => {
                         {errors.rating?.type === "max" && <p style={{color:"red",fontSize:"20px"}}> sorry You cannot given rating above 5 </p> }
                         {errors.rating?.type === "min" &&  alert("hi")}
 
-                        <input className='inputReview' type="submit" />
+                        <input  className='inputReview' type="submit" />
                     </form>
 
                   
